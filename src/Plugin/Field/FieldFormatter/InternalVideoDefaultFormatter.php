2ffde856f284041f88df4e996af1e35d395369a9
usmanjutt84 <usmanssasoft@gmail.com>
2024-06-23 22:07:51 +0500

    Updated the field formatter to enable tracking.

--- src/Plugin/Field/FieldFormatter/InternalVideoDefaultFormatter.php
+++ src/Plugin/Field/FieldFormatter/InternalVideoDefaultFormatter.php
@@ -5,6 +5,8 @@ namespace Drupal\internal_video\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Field\FieldItemListInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\internal_video\Plugin\Field\FieldType\InternalVideo;
 
 /**
  * Plugin implementation of the 'internal_video' formatter.
@@ -18,6 +20,31 @@ use Drupal\Core\Field\FieldItemListInterface;
  * )
  */
 class InternalVideoDefaultFormatter extends FormatterBase {
+
+  /**
+   * The configs.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
+   * The The tracking service.
+   *
+   * @var \Drupal\internal_video\Controller\Tracking
+   */
+  protected $tracking;
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
+    $instance->config = $container->get('config.factory')->get('internal_video.settings');
+    $instance->tracking = $container->get('internal_video.tracking');
+    return $instance;
+  }
 
   /**
    * {@inheritdoc}
@@ -74,14 +101,36 @@ class InternalVideoDefaultFormatter extends FormatterBase {
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = [];
+    $tracking_enabled = $items->getFieldDefinition()->getSettings();
     $settings = $this->getSettings();
+    $is_login = \Drupal::currentUser()->isAuthenticated();
 
     foreach ($items as $delta => $item) {
+      // Get tracking info if video tracking is enabled and user is authenticated.
+      $tracking_info = $is_login && $tracking_enabled ? $this->getTrackingInfo($item) : [];
+      // Find if it's already tracked
+      $already_tracked = $tracking_info ? $this->tracking->findTracking($tracking_info) : false;
+      /**
+       * Generate unique id for tracking only when:
+       *  - video tracking is enabled
+       *  - user is authenticated
+       *  - video is not yet tracked
+       */
+      $unique_id = $tracking_info && !$already_tracked ? uniqid() : null;
+
+      $src = $item->value;
+      $headers = get_headers($src, true);
+      $mime_type = $headers['Content-Type'];
+
       $element[$delta] = [
         '#theme' => 'internal_video',
-        '#value' => $item->value,
+        '#video' => [
+          'src' => $src,
+          'mime_type' => $mime_type,
+        ],
         '#entity_title' => $items->getEntity()->label(),
         '#settings' => $settings,
+        '#tracking_id' => $unique_id,
       ];
 
       // Attach videoJS to the element
@@ -91,13 +140,45 @@ class InternalVideoDefaultFormatter extends FormatterBase {
       if ($theme = $settings['video_theme']) {
         $element[$delta]['#attached']['library'][] = 'internal_video/internal_video.theme.'.$theme;
       }
-
-
-      if ($settings['video_size'] == 'responsive') { // TODO: remove?
-        // $element[$delta]['#attached']['library'][] = 'youtube/drupal.youtube.responsive';
+      // if video tracking is enabled unique_id is created
+      if($tracking_enabled && $unique_id) {
+        $element[$delta]['#attached']['drupalSettings']['internal_video_tracking'][$unique_id] = $tracking_info;
+        // Add wait time for video tracking
+        $element[$delta]['#attached']['drupalSettings']['internal_video_tracking']['wait_time'] = $this->config->get('wait_time');
+        // Attach tracking to the element
+        $element[$delta]['#attached']['library'][] = 'internal_video/internal_video.tracking';
       }
     }
+
     return $element;
+  }
+
+  /**
+   * Get tracking information to add in the video attribute
+   *
+   * @param InternalVideo $field
+   * @return array
+   */
+  protected function getTrackingInfo(InternalVideo $field) {
+    $curren_user = \Drupal::currentUser();
+    if(!$curren_user->isAuthenticated()) return [];
+
+    $field_definition = $field->getFieldDefinition();
+    $uid = $curren_user->id();
+    $entity_type = $field_definition->getTargetEntityTypeId();
+    $entity_bunde = $field_definition->getTargetBundle();
+    $entity_id = $field->getEntity()->id();
+    $field_name = $field_definition->getName();
+    $video_uri = $field->value;
+
+    return [
+      'uid' => $uid,
+      'entity_type' => $entity_type,
+      'entity_bundle' => $entity_bunde,
+      'entity_id' => $entity_id,
+      'field_name' => $field_name,
+      'video_uri' => $video_uri,
+    ];
   }
 
   /**
